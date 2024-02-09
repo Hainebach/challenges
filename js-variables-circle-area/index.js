9dce839e8097388ba15b9e68d32385d34d5fff54
Yoav Hainebach <yoavhainebach@gmail.com>
2024-02-09 16:41:43 +0100

    circumference calculator

---
+++ js-variables-circle-area/index.js
@@ -0,0 +1,9 @@
+const circleRadius = 24;
+
+let circleArea = Math.PI * Math.pow(circleRadius, 2);
+
+// console.log(circleArea);
+
+let circumference = 2 * Math.PI * circleRadius;
+
+console.log(circumference);
