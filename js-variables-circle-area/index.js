3374e6626f21cfb4df54329a0227c9cc3c0583d2
Yoav Hainebach <yoavhainebach@gmail.com>
2024-02-11 15:06:48 +0100

    adding the correct console log

--- js-variables-circle-area/index.js
+++ js-variables-circle-area/index.js
@@ -1,4 +1,4 @@
-const circleRadius = 24;
+const circleRadius = 5;
 
 let circleArea = Math.PI * Math.pow(circleRadius, 2);
 
@@ -6,4 +6,17 @@ let circleArea = Math.PI * Math.pow(circleRadius, 2);
 
 let circumference = 2 * Math.PI * circleRadius;
 
-console.log(circumference);
+// console.log(circumference);
+
+console.log("For a circle with a radius of " + circleRadius + " units:");
+console.log(
+  "The area is approximately " + circleArea.toFixed(2) + " square units."
+);
+console.log(
+  "The circumference is approximately " + circumference.toFixed(2) + " units."
+);
+
+// For a circle with a radius of 5 units:
+
+// - The area is approximately 78.54 square units.
+// - The circumference is approximately 31.42 units.
