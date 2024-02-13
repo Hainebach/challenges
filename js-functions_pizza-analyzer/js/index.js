3e738759d02835859c48df51c95e6edd427a5524
Yoav Hainebach <yoavhainebach@gmail.com>
2024-02-13 16:55:08 +0100

    part 1

--- js-functions_pizza-analyzer/js/index.js
+++ js-functions_pizza-analyzer/js/index.js
@@ -9,15 +9,29 @@ const outputSection = document.querySelector('[data-js="output-section"]');
 const output = document.querySelector('[data-js="output"]');
 
 pizzaInput1.addEventListener("input", () => {
-  // write your code here
+  let pizzaSize1 = pizzaInput1;
+  let pizzaSize2 = pizzaInput2;
+
+  calculatePizzaGain(pizzaSize2.value, pizzaSize1.value);
 });
 
 pizzaInput2.addEventListener("input", () => {
-  // write your code here
+  let pizzaSize2 = pizzaInput2;
+  let pizzaSize1 = pizzaInput1;
+
+  calculatePizzaGain(pizzaSize1.value, pizzaSize2.value);
 });
 
 // Task 1
 // define the function calculatePizzaGain here
+function calculatePizzaGain(diameter1, diameter2) {
+  const area1 = Math.PI * Math.pow(diameter1 / 2, 2);
+  const area2 = Math.PI * Math.pow(diameter2 / 2, 2);
+  output.textContent = Math.round(((area2 - area1) / area1) * 100);
+  // return ((area2 - area1) / area1) * 100;
+}
+
+// output.textContent = Math.round(calculatePizzaGain());
 
 // Task 2
 // define the function updatePizzaDisplay here
