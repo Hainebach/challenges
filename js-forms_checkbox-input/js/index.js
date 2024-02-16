b0b84e90954e9f08ebf16079c22526bc8ef56218
Yoav Hainebach <yoavhainebach@gmail.com>
2024-02-16 13:56:30 +0100

    started

--- js-forms_checkbox-input/js/index.js
+++ js-forms_checkbox-input/js/index.js
@@ -3,6 +3,7 @@ console.clear();
 const form = document.querySelector('[data-js="form"]');
 const tosError = document.querySelector('[data-js="tos-error"]');
 const tosCheckbox = document.querySelector('[data-js="tos"]');
+const success = document.querySelector('[data-js="success"]');
 
 function hideTosError() {
   tosError.setAttribute("hidden", "");
@@ -12,10 +13,23 @@ function showTosError() {
   tosError.removeAttribute("hidden");
 }
 
+function showSuccess() {
+  success.removeAttribute("hidden");
+}
+
+function hideSuccess() {
+  success.removeAttribute("hidden", "");
+}
+
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   // --v-- write your code here --v--
+  const formData = new FormData(evt.target);
+  const userData = Object.fromEntries(formData);
+  Boolean checked = 
+
+  // hideSuccess();
 
   // --^-- write your code here --^--
 
