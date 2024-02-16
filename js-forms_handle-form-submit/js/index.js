f8ded7b0aba128b65fc1efd80628f9150aad540a
Yoav Hainebach <yoavhainebach@gmail.com>
2024-02-16 11:04:37 +0100

    first part

--- js-forms_handle-form-submit/js/index.js
+++ js-forms_handle-form-submit/js/index.js
@@ -1,3 +1,13 @@
 console.clear();
 
 const form = document.querySelector('[data-js="form"]');
+
+form.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+
+  const formData = new FormData(evt.target);
+  const userData = Object.fromEntries(formData);
+
+  console.log(userData);
+  form.reset();
+});
