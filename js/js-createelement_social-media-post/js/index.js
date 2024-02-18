f241e619c29551d6bac2ca4880c954ca140d6aad
Yoav Hainebach <yoavhainebach@gmail.com>
2024-02-18 19:11:55 +0100

    like

--- js/js-createelement_social-media-post/js/index.js
+++ js/js-createelement_social-media-post/js/index.js
@@ -10,3 +10,30 @@ likeButton.addEventListener("click", handleLikeButtonClick);
 
 // Exercise:
 // Use document.createElement() and append another social media post to the body.
+const body = document.querySelector('[data-js="body"]');
+
+const newPostSection = document.createElement("section");
+newPostSection.classList.add("post");
+body.appendChild(newPostSection);
+
+const newPost = document.createElement("p");
+newPost.textContent =
+  " Lorem ipsum dolor, sit amet consectetur adipisicing elit. ";
+newPost.classList.add("post__content");
+newPostSection.appendChild(newPost);
+
+const newFooter = document.createElement("footer");
+newFooter.classList.add("post__footer");
+newPostSection.appendChild(newFooter);
+
+const newSpan = document.createElement("span");
+newSpan.classList.add("post__username");
+newSpan.textContent = "@username";
+newFooter.appendChild(newSpan);
+
+const newButton = document.createElement("button");
+newButton.classList.add("post__button");
+newButton.textContent = "♥ Like";
+newFooter.appendChild(newButton);
+
+newButton.addEventListener("click", handleLikeButtonClick);
