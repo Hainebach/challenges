6b713879db532e90fcf6fc923c1b4bdaf54c7b7c
Yoav Hainebach <yoavhainebach@gmail.com>
2024-02-20 16:40:20 +0100

    render stars

--- js-loops_stars/js/index.js
+++ js-loops_stars/js/index.js
@@ -8,6 +8,13 @@ function renderStars() {
 
   //--v-- your code here --v--
 
+  for (let i = 1; i <= 5; i++) {
+    const img = document.createElement("img");
+    img.setAttribute("src", "assets/star-empty.svg");
+    starContainer.appendChild(img);
+    console.log(img);
+  }
+
   //--^-- your code here --^--
 }
 
