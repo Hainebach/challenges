761aadc19380c1c45785bbd41b9cc46d8e7a2760
Yoav Hainebach <yoavhainebach@gmail.com>
2024-02-21 10:22:26 +0100

    welcome

--- js-callback-functions_user-login-callback-1-named-vs-anonymous/js/index.js
+++ js-callback-functions_user-login-callback-1-named-vs-anonymous/js/index.js
@@ -5,4 +5,17 @@ function handleUserLogin(onSuccess) {
   onSuccess();
 }
 
+function showWelcomeMessage() {
+  const welcomeMessage = "Welcome! You are logged in now.";
+  console.log(welcomeMessage);
+  return welcomeMessage;
+}
+
+showWelcomeMessage("hello");
 // Call handleUserLogin below!
+
+handleUserLogin(() => {
+  const welcomeMessage = "Welcome You are now logged in too";
+  console.log(welcomeMessage);
+  return welcomeMessage;
+});
