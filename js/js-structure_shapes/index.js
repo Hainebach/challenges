4ee7b38c1abc627d4b174841ffd42497c0ac9bfa
Yoav Hainebach <yoavhainebach@gmail.com>
2024-02-23 11:27:25 +0100

    seperate all componenets to different js files

--- js/js-structure_shapes/index.js
+++ js/js-structure_shapes/index.js
@@ -3,16 +3,12 @@ console.clear();
 import { getRandomColor } from "./utils/randomColor.js";
 import { Circle } from "./components/Circle/circle.js";
 import { Pentagon } from "./components/Pentagon/pentagon.js";
+import { Square } from "./components/Square/square.js";
 
 const root = document.getElementById("root");
 
-const square = document.createElement("div");
-square.classList.add("square");
-square.addEventListener("click", () => {
-  square.style.backgroundColor = getRandomColor();
-});
-
 const circle = Circle();
 const pentagon = Pentagon();
+const square = Square();
 
 root.append(circle, square, pentagon);
