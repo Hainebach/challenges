4406deccba15c5ef73b325453232a628117b79b6
Yoav Hainebach <yoavhainebach@gmail.com>
2024-02-27 13:42:44 +0100

    create fetchData function

--- js/js-fetch_star-wars-console/js/index.js
+++ js/js-fetch_star-wars-console/js/index.js
@@ -2,6 +2,31 @@ console.clear();
 
 const url = "https://swapi.dev/api/people";
 
-function fetchData() {}
+async function fetchData() {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      console.error("bad request");
+      return null;
+    }
 
-fetchData();
+    const data = await response.json();
+
+    return data;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
+
+// (async () => {
+//   const data = await fetchData();
+//   console.log(data.results[2]);
+// })();
+
+const data = await fetchData();
+console.log(data.results[0].eye_color);
+console.log(data.results[4].eye_color);
+console.log(
+  `name: ${data.results[5].name}, skin color: ${data.results[5]["skin_color"]}`
+);
