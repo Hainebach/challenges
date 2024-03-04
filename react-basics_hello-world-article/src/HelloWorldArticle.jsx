321b701a306cd66be0033d5458d9bf65ad6c7d24
Yoav Hainebach <yoavhainebach@gmail.com>
2024-03-04 15:50:34 +0100

    create an article element

---
+++ react-basics_hello-world-article/src/HelloWorldArticle.jsx
@@ -0,0 +1,9 @@
+export default function HelloWorldArticle() {
+  return (
+    <article>
+      <h1>Hello World!</h1>
+      <p>hello paragraph</p>
+      <p>hello second paragraph</p>
+    </article>
+  );
+}
