2bd3e04f2595c542b913478045b82be3ca77e571
Yoav Hainebach <yoavhainebach@gmail.com>
2024-03-05 16:32:22 +0100

    create Logo and Avatar components

--- react-nesting_navigation/src/App.jsx
+++ react-nesting_navigation/src/App.jsx
@@ -2,26 +2,20 @@ import "./App.css";
 import Header from "./components/Header.jsx";
 import Navigation from "./components/Navigation.jsx";
 import Link from "./components/Link.jsx";
-
-import avatar from "./assets/avatar.jpg";
+import Logo from "./components/Logo.jsx";
+import Avatar from "./components/Avatar.jsx";
 
 export default function App() {
   return (
     <>
       <Header>
-        {" "}
+        <Logo />
         <Navigation>
           <Link href="#home">Home</Link>
           <Link href="#about">About</Link>
           <Link href="#impressum">Impressum</Link>
         </Navigation>
-        <button
-          type="button"
-          onClick={() => console.log("I could toggle a profile!")}
-          aria-label="toggle profile"
-        >
-          <img className="round-image" src={avatar} alt="avatar" />
-        </button>
+        <Avatar />
       </Header>
 
       <main>content goes here…</main>
--- react-nesting_navigation/src/components/Header.jsx
+++ react-nesting_navigation/src/components/Header.jsx
@@ -1,14 +1,7 @@
-import logo from "/Users/yoavhainebach/spiced/spiced-bootcamp/challenges/react-nesting_navigation/src/assets/logo.jpg";
-
 export default function Header({ children }) {
   return (
     <>
-      <header className="header">
-        <a href="#">
-          <img className="round-image" src={logo} alt="logo" />
-        </a>
-        {children}
-      </header>
+      <header className="header">{children}</header>
     </>
   );
 }
