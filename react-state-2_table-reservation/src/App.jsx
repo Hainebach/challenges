f656c67d70f939827bbc6bb8745e4684c3f855d2
Yoav Hainebach <yoavhainebach@gmail.com>
2024-03-11 11:08:19 +0100

    lift up the people state to main

--- react-state-2_table-reservation/src/App.jsx
+++ react-state-2_table-reservation/src/App.jsx
@@ -1,12 +1,20 @@
 import Counter from "./components/Counter";
 import "./App.css";
+import { useState } from "react";
 
 export default function App() {
+  function handleIncrease() {
+    setPeople(people + 1);
+  }
+  function handleDecrease() {
+    setPeople(people - 1);
+  }
+  const [people, setPeople] = useState(0);
   return (
     <div className="container">
       <h1>Place a Table Reservation</h1>
-      <Counter />
-      <p>You are going to reserve a table for 2 people.</p>
+      <Counter onIncrease={handleIncrease} onDecrease={handleDecrease} />
+      <p>You are going to reserve a table for {people} people.</p>
     </div>
   );
 }
