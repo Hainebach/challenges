10e7bbaf7c7fcaeac8d9488fc62e1f9965419347
Yoav Hainebach <yoavhainebach@gmail.com>
2024-03-11 15:55:37 +0100

    add new animal to the array

--- react-state-3_adding-animals/src/App.jsx
+++ react-state-3_adding-animals/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Form from "./components/Form";
 import List from "./components/List";
+import { uid } from "uid";
 import "./App.css";
 
 const initialAnimals = [
@@ -26,6 +27,7 @@ export default function App() {
 
   function handleAddAnimal(newAnimal) {
     console.log(newAnimal);
+    setAnimals([...animals, { ...newAnimal, id: uid() }]);
   }
 
   return (
