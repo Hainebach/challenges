18232ebf4800c4b350940cb918da48fb0deae750
Yoav Hainebach <yoavhainebach@gmail.com>
2024-03-12 10:47:15 +0100

    add useEffect to load list when the app renders

--- react-effects-and-fetch_pokemon-api/src/components/PokemonList.jsx
+++ react-effects-and-fetch_pokemon-api/src/components/PokemonList.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
+import { useEffect } from "react";
 import "./PokemonList.css";
-
 
 export default function PokemonList() {
   const [pokemon, setPokemon] = useState([]);
@@ -15,14 +15,20 @@ export default function PokemonList() {
     }
   }
 
+  useEffect(() => {
+    loadPokemon();
+  }, []);
+
   return (
     <main>
-      <button type="button" className="button" onClick={loadPokemon}>
+      {/* <button type="button" className="button" onClick={loadPokemon}>
         Load Pokémon
-      </button>
+      </button> */}
       <ul>
         {pokemon.map(({ name }) => (
-          <li key={name} className="pokemon">{name}</li>
+          <li key={name} className="pokemon">
+            {name}
+          </li>
         ))}
       </ul>
     </main>
