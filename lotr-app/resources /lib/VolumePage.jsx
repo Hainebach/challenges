289259646b4fe21b7a92fec244503bcdd06f45be
Yoav Hainebach <yoavhainebach@gmail.com>
2024-03-18 14:37:11 +0100

    add image to every volume page

--- lotr-app/resources /lib/VolumePage.jsx
+++ lotr-app/resources /lib/VolumePage.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Link from "next/link";
-import { volumes } from "@/resources /lib/data";
+import Image from "next/image";
 
 export default function VolumePage({ volume }) {
   return (
     <>
-      <Link href="/">All Volumes</Link>;<h1>{volume.title}</h1>
+      <Link href="/">All Volumes</Link>
+      <h1>{volume.title}</h1>
       <p>{volume.description}</p>
       <ul>
         <h3>books:</h3>
@@ -16,6 +17,7 @@ export default function VolumePage({ volume }) {
           </li>
         ))}
       </ul>
+      <Image src={volume.cover} alt={volume.title} width={144} height={230} />
     </>
   );
 }
