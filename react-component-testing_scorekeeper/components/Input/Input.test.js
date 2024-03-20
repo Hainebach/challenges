8b8b37b3b51118c05f98aca18b3b5e8150c8283a
Yoav Hainebach <yoavhainebach@gmail.com>
2024-03-20 13:40:57 +0100

    test for creating label with the correct attributes

--- react-component-testing_scorekeeper/components/Input/Input.test.js
+++ react-component-testing_scorekeeper/components/Input/Input.test.js
@@ -2,6 +2,13 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import Input from ".";
 
-test("renders a label and an input with the correct attributes", () => {});
+test("renders a label and an input with the correct attributes", () => {
+  render(<Input placeholder={""} name={""} required type="text" />);
+  const inputElement = screen.getByRole("textbox");
+  expect(inputElement).toHaveAttribute("placeholder");
+  expect(inputElement).toHaveAttribute("name");
+  expect(inputElement).toHaveAttribute("required");
+  expect(inputElement).toHaveAttribute("type", "text");
+});
 
 test("calls callback on every user input", async () => {});
