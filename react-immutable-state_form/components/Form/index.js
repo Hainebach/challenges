69dec556bbb404d71644d1450ef8de57ef7eb215
Yoav Hainebach <yoavhainebach@gmail.com>
2024-03-22 11:15:03 +0100

    add logic to the three functions changing the form values

--- react-immutable-state_form/components/Form/index.js
+++ react-immutable-state_form/components/Form/index.js
@@ -10,11 +10,26 @@ export default function Form() {
     },
   });
 
-  function handleNameChange(event) {}
+  function handleNameChange(event) {
+    const newName = event.target.value;
+    setMountain((draft) => {
+      draft.name = newName;
+    });
+  }
 
-  function handleAltitudeChange(event) {}
+  function handleAltitudeChange(event) {
+    const newAltitude = event.target.value;
+    setMountain((draft) => {
+      draft.values.altitude = newAltitude;
+    });
+  }
 
-  function handleMountainRangeChange(event) {}
+  function handleMountainRangeChange(event) {
+    const newRange = event.target.value;
+    setMountain((draft) => {
+      draft.values.mountainRange = newRange;
+    });
+  }
 
   return (
     <StyledForm>
