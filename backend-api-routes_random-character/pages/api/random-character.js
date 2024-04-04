ceaf765ebe4da5c64d0240023d9a26e70122e2aa
Yoav Hainebach <yoavhainebach@gmail.com>
2024-04-04 13:46:52 +0200

    create random-character api route

---
+++ backend-api-routes_random-character/pages/api/random-character.js
@@ -0,0 +1,13 @@
+import Chance from "chance";
+
+export default function handler(request, response) {
+  const chance = new Chance();
+  const character = {
+    firstName: chance.first(),
+    lastName: chance.last(),
+    phoneNumber: chance.phone(),
+    birthday: chance.birthday({ string: true, american: false }),
+    address: chance.address({ short_suffix: true }),
+  };
+  response.status(200).json(character);
+}
--- backend-api-routes_random-character/pages/index.js
+++ backend-api-routes_random-character/pages/index.js
@@ -1,3 +1,7 @@
+import useSWR from "swr";
+
+const fetcher = (url) => fetch(url).then((res) => res.json());
+
 export default function HomePage() {
   return <h1>Hello from Next.js!</h1>;
 }
